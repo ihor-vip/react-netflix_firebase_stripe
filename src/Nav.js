de0d0206447124577f0adc04b3ef4d1fc6c89c9e
ihor-vip <vip20140902@gmail.com>
2023-02-14 16:22:59 +0200

    adding scrolling function to header

---
+++ src/Nav.js
@@ -0,0 +1,37 @@
+import {useEffect, useState} from "react";
+import './Nav.css';
+
+export default function Nav() {
+    const [show, handleShow] = useState(false);
+
+    const transitionForNavBar = () => {
+        if (window.scrollY > 100) {
+            handleShow(true)
+        } else {
+            handleShow(false)
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', transitionForNavBar);
+        return () => window.removeEventListener('scroll', transitionForNavBar)
+    },[])
+
+    return (
+        <div className={`nav ${show && 'nav_black'}`}>
+            <div className="nav_contents">
+                <img
+                    className='nav_logo'
+                    src='https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png'
+                    alt='netflix_logo'
+                />
+
+                <img
+                    className='nav_avatar'
+                    src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8n9HeEUrq7Pj9P0advGP3thHejzAB2_TRNfgpmlscnA6RL4ghAH3GMv3lpMkV3BfX2hI&usqp=CAU'
+                    alt='netflix_avatar'
+                />
+            </div>
+        </div>
+    );
+}
