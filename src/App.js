bf56d5b52a7a460f8139fa0c9d92a97b7063d83f
ihor-vip <vip20140902@gmail.com>
2023-02-15 13:10:21 +0200

    work on login screen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import './App.css';
-import HomeScreen from "./HomeScreen";
+import HomeScreen from "./screens/HomeScreen/HomeScreen";
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+import LoginScreen from "./screens/LoginScreen/LoginScreen";
 
 function App() {
+    const router = createBrowserRouter([
+        {
+            path: "/",
+            element: <div className='app'><HomeScreen/></div>,
+        },
+        {
+            path: '/login',
+            element: <div><LoginScreen/></div>
+        }
+    ]);
+
     return (
-        <div className="app">
-            <HomeScreen/>
+        <div>
+            <RouterProvider router={router} />
         </div>
     );
 }
