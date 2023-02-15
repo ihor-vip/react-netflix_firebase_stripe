703e21171e42054decfd7a739ddb6341fe360f15
ihor-vip <vip20140902@gmail.com>
2023-02-15 15:20:27 +0200

    building the signup screen component, implementing firestore

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import HomeScreen from "./screens/HomeScreen/HomeScreen";
 import {
@@ -6,8 +6,25 @@ import {
     RouterProvider,
 } from "react-router-dom";
 import LoginScreen from "./screens/LoginScreen/LoginScreen";
+import {auth} from "./firebase";
 
 function App() {
+    const user = null;
+
+    useEffect(() => {
+       const unsubscribe =  auth.onAuthStateChanged(userAuth => {
+            if (userAuth) {
+
+            } else {
+
+            }
+        })
+        return () => {
+           unsubscribe()
+        }
+
+    },[])
+
     const router = createBrowserRouter([
         {
             path: "/",
